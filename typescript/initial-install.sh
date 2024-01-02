d21ead081e3ff9d22b2e0b6cf203d7242d557710
Ubugeeei <ubuge1122@gmail.com>
2024-01-02 22:54:16 +0900

    add initial install script

---
+++ typescript/initial-install.sh
@@ -0,0 +1,7 @@
+#! remove this file after initial install
+
+# dependencies
+# pnpm install
+
+# dev dependencies
+pnpm install -D @types/dts-bundle @types/node chainsi dts esbuild oxlint rimraf tsx typescript vitest

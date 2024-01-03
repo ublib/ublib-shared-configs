5a7004918c9eadc5659b06bedf6f5abc31809e97
Ubugeeei <ubuge1122@gmail.com>
2024-01-03 13:13:07 +0900

    add husky to initial install script

--- typescript/initial-install.sh
+++ typescript/initial-install.sh
@@ -4,4 +4,4 @@
 # pnpm install
 
 # dev dependencies
-pnpm install -D @types/dts-bundle @types/node chainsi dts-bundle esbuild oxlint rimraf tsx typescript vitest
+pnpm install -D @types/dts-bundle @types/node chainsi dts-bundle esbuild husky oxlint rimraf tsx typescript vitest

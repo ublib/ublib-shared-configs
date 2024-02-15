eebf62a55ae70ba9d5ac5a6c8d5a49f375823b7b
Ubugeeei <ubuge1122@gmail.com>
2024-02-15 22:16:34 +0900

    add w flag

--- typescript/initial-install.sh
+++ typescript/initial-install.sh
@@ -4,4 +4,4 @@
 # pnpm install
 
 # dev dependencies
-pnpm install -D @biomejs/biome @types/dts-bundle @types/node chainsi dts-bundle esbuild husky oxlint rimraf tsx typescript vitest
+pnpm install -Dw @biomejs/biome @types/dts-bundle @types/node chainsi dts-bundle esbuild husky oxlint rimraf tsx typescript vitest
